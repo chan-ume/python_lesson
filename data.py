c7479036be77a6f83648d307db6881263244d9dd
chan_ume <dottobasic673@gmail.com>
2016-03-15 10:42:15 +0000

    practice data

---
+++ data.py
@@ -0,0 +1,13 @@
+# coding: UTF-8
+a = 10
+b = 1.234234
+c = "taguchi"
+d = {"fkoji":200, "dointstall":500}
+print "age: %d" % a
+print "age: %10d" % a
+print "age: %010d" % a
+print "price: %f" % b
+print "price: %.2f" % b
+print "name: %s" % c
+print "sales: %(fkoji)d" % d
+print "%d and %f" % (a, b)
