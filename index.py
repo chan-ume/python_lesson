6d30bdd081662e3287e0de429c233ac82afe54e2
chan_ume <dottobasic673@gmail.com>
2016-03-15 08:32:38 +0000

    practice moziretsu

--- index.py
+++ index.py
@@ -1,1 +1,17 @@
+# coding: UTF-8
+# 文字列
 print "hello world"
+
+# 日本語　u"こんにちは！"
+
+print "hello" + "world"
+
+print u"無駄"*10
+
+print "改行\n改行\tタブ\\ it\'s a pen"
+
+
+print """<html lang="ja">
+<body>
+</body>
+<html>"""
---
+++ second.py
@@ -0,0 +1,5 @@
+# coding: UTF-8
+s ="acasdf"
+print len(s)
+print s.find("c")
+
