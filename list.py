c17cd903152325ddf6e05e2d98121c4029ed565c
chan_ume <dottobasic673@gmail.com>
2016-03-15 08:39:41 +0000

    practice list

---
+++ list.py
@@ -0,0 +1,14 @@
+# coding:UTF-8
+#リスト
+
+sales = [255,100, 353,400]
+print len(sales)
+print sales[2]
+sales[2]=100
+print sales[2]
+
+# in
+print 100 in sales #True or false
+# range
+print range(10)
+print range(3,10,2)
